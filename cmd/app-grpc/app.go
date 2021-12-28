a5fdfec80252eb7752b772f52aa5c3b070d7b51f
Husein Nashr <nvdhunter007@gmail.com>
2021-12-28 15:12:23 +0700

    load yaml

--- cmd/app-grpc/app.go
+++ cmd/app-grpc/app.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"fmt"
+
+	"github.com/TheWokeDeveloper/budgetme-backend/internal/config"
 	serverRepo "github.com/TheWokeDeveloper/budgetme-backend/internal/repo/server"
 	serverUseCase "github.com/TheWokeDeveloper/budgetme-backend/internal/usecase/server"
+	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
-func startApp() {
+func startApp(cfg *config.Config) {
 	metrics, err := newrelic.NewApplication(
 		newrelic.ConfigAppName("budgetme-backend"),
 		newrelic.ConfigLicense("5d45054992c7f69d5dd9bda28b4005f676a4NRAL"),
@@ -16,8 +20,8 @@ func startApp() {
 		panic(fmt.Sprintf("Failed to initialize newrelic app. %v", err))
 	}
 
-	serverRepo := serverRepo.New()
+	serverRepo := serverRepo.New(cfg)
 	serverUseCase := serverUseCase.New(serverRepo)
 
-	startServer(metrics, serverUseCase)
+	startServer(cfg, metrics, serverUseCase)
 }
--- internal/repo/server/repo.server.method.go
+++ internal/repo/server/repo.server.method.go
@@ -4,9 +4,8 @@ import (
 	"fmt"
 
 	ServerEntity "github.com/TheWokeDeveloper/budgetme-backend/internal/entity/server"
-	"github.com/TheWokeDeveloper/budgetme-backend/internal/pkg/env"
 )
 
 func (r *Repo) GetHealth() (*ServerEntity.Health, error) {
-	return &ServerEntity.Health{Message: fmt.Sprintf("Server running on %s", env.Get("version", "@latest"))}, nil
+	return &ServerEntity.Health{Message: fmt.Sprintf("Server running on %s", r.cfg.Version)}, nil
 }
