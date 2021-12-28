e5cdfc92721d5c2b19290487562d54672df6fb2f
Husein Nashr <nvdhunter007@gmail.com>
2021-12-28 12:30:10 +0700

    implement new relic apm

--- cmd/app-grpc/app.go
+++ cmd/app-grpc/app.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"fmt"
+
 	serverRepo "github.com/TheUnderdogFolks/budgetme-backend/internal/repo/server"
 	serverUseCase "github.com/TheUnderdogFolks/budgetme-backend/internal/usecase/server"
+	"github.com/newrelic/go-agent/v3/newrelic"
 )
 
 func startApp() {
-	serverRepo := serverRepo.New()
+	metrics, err := newrelic.NewApplication(
+		newrelic.ConfigAppName("budgetme-backend"),
+		newrelic.ConfigLicense("5d45054992c7f69d5dd9bda28b4005f676a4NRAL"),
+		newrelic.ConfigDistributedTracerEnabled(true),
+	)
 
+	if err != nil {
+		panic(fmt.Sprintf("Failed to initialize newrelic app. %v", err))
+	}
+
+	serverRepo := serverRepo.New()
 	serverUseCase := serverUseCase.New(serverRepo)
 
-	startServer(serverUseCase)
+	startServer(metrics, serverUseCase)
 }
--- go.mod
+++ go.mod
@@ -3,12 +3,16 @@ module github.com/TheUnderdogFolks/budgetme-backend
 go 1.17
 
 require (
-	github.com/gogo/protobuf v1.3.2 // indirect
-	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/protobuf v1.5.2
+	github.com/newrelic/go-agent/v3 v3.15.2
+	google.golang.org/grpc v1.43.0
+)
+
+require (
+	github.com/newrelic/go-agent/v3/integrations/nrgrpc v1.3.1
 	golang.org/x/net v0.0.0-20210405180319-a5a99cb37ef4 // indirect
 	golang.org/x/sys v0.0.0-20210510120138-977fb7262007 // indirect
 	golang.org/x/text v0.3.6 // indirect
 	google.golang.org/genproto v0.0.0-20211208223120-3a66f561d7aa // indirect
-	google.golang.org/grpc v1.43.0 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 )
